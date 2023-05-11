5187709709af917fafc6fc0d987c06ee91599b95
Tatiana <tanya_13.88@mail.ru>
2023-05-11 13:47:56 +0300

    added firebase

--- src/pages/Todo.vue
+++ src/pages/Todo.vue
@@ -10,6 +10,7 @@
     </div>
     <div class="q-pa-md" style="max-width: 600px">
       <q-form
+        @submit.prevent="addTask"
         @keyup.enter="addTask"
         class="q-gutter-md flex flex-center"
       >
@@ -20,26 +21,41 @@
           lazy-rules
           :rules="[ val => val && val.length > 0 || 'Please type something']"
         />
-          <q-btn @click="addTask" label="Add task" type="submit" color="dark" class="add_btn"/>
+          <q-btn 
+          :disabled="!newTask"
+          @click="addTask" 
+          label="Add task" 
+          type="submit" 
+          color="dark" 
+          class="add_btn"/>
       </q-form>
    
       <q-list separator>
         <q-item 
-          @click="task.done = !task.done"
+      
+          @click="toggleDone(task.id)"
           clickable
           :class="{ 'done bg-pink-1': task.done }"
-          v-for="(task, index) in tasks" 
-          :key="task.title" 
+          v-for="task in tasks" 
+          :key="task.id" 
           v-ripple>
           <q-item-section avatar>
-            <q-checkbox class="no-pointer-events"
-            v-model="task.done" val="teal" color="dark" />
+            <q-checkbox 
+              class="no-pointer-events"
+              v-model="task.done" 
+              val="teal" 
+              color="dark" />
           </q-item-section>
           <q-item-section>
             <q-item-label>{{ task.title }}</q-item-label>
           </q-item-section>
           <q-item-section v-if="task.done" side> 
-            <q-btn @click.stop="deleteTask(index)" size="10px" round color="dark" icon="delete_forever" />
+            <q-btn 
+              @click.stop="deleteTask(task.id)" 
+              size="10px" 
+              round color="dark" 
+              icon="delete_forever" 
+              />
           </q-item-section>
         </q-item>
       </q-list>
@@ -53,73 +69,94 @@
   </q-page>
 </template>
 
-<script>
-import { defineComponent } from 'vue'
+<script setup>
+// import { defineComponent } from 'vue';
+import { db } from "../firebase";
+import { useQuasar } from "quasar";
+import { ref, onMounted } from 'vue';
+import { 
+  collection, 
+  onSnapshot, 
+  addDoc, 
+  doc, 
+  deleteDoc, 
+  updateDoc,
+  query, 
+  orderBy,
+ } from "firebase/firestore";
+ 
+ //firebase ref 
+const tasksCollectionRef = collection(db, "tasks");
+const tasksCollectionQuery = query(tasksCollectionRef, orderBy("date", "desc"));
 
-export default defineComponent({
-  name: 'IndexPage',
-  data() {
-    return {
-      newTask: "",
-      tasks:[
-        // {
-        //   title: "Go to shop",
-        //   done: false,
-        // },
-        // {
-        //   title: "Go to the pool",
-        //   done: false,
-        // },
-        // {
-        //   title: "Sign up for dancing",
-        //   done: false,
-        // },
-        // {
-        //   title: "Do homework",
-        //   done: false,
-        // },
-        // {
-        //   title: "Go to museum or zoo",
-        //   done: false,
-        // },
-      ],
-    };
-  },
-  methods: {
-    deleteTask(index) {
-      this.$q.dialog({
-        title: 'Attention!',
-        message: 'Are you sure you want to delete task?',
-        cancel: true,
-        persistent: true
-      }).onOk(() => {
-        this.tasks.splice(index,1);
-        this.$q.notify({
-          type: "negative", 
-          message: 'Task deleted'
-        })
-      })
-    },
-    addTask() {
-      if (this.newTask) {
-        this.tasks.push({
-          title: this.newTask,
-          done: false
-        });
-        this.newTask = "";
-        this.$q.notify({
-          type: "positive", 
-          message: 'Task added'
+//todoList
+const tasks = ref ([]);
+const newTask = ref("");
+
+const $q = useQuasar();
+
+//get tasks
+onMounted(async () => {
+  onSnapshot(tasksCollectionQuery, (querySnapshot) => {
+    const fbTasks = [];
+    querySnapshot.forEach((doc) => {
+      const task = {
+        id: doc.id,
+        title: doc.data().title,
+        done: doc.data().done,
+    }; 
+    fbTasks.push(task); 
+    });
+    tasks.value = fbTasks;
+  });
+})
+const addTask = async () => {
+  if (newTask.value.trim() === "") {
+        $q.notify({
+          type: "negative",
+          message: "Your task is empty",
+        }); 
+  } else {
+      addDoc(tasksCollectionRef, {
+        title: newTask.value,
+        done: false,
+        date: Date.now(),
+      });
+        newTask.value = "";
+        $q.notify({
+          type: "positive",
+          message: "Your task has been added.",
         });
       }
     }
-  }
-})
+//delete todo
+const deleteTask = async (id) => {
+  $q.dialog({
+    title: 'Attention!',
+    message: 'Are you sure you want to delete task?',
+    cancel: true,
+    persistent: true
+}).onOk(() => {
+  deleteDoc(doc(tasksCollectionRef,id));
+    $q.notify({
+      type: "negative", 
+      message: 'Task deleted'
+  })
+ })
+}
+//toggler
+const toggleDone = (id) => {
+  const index = tasks.value.findIndex((task) => task.id === id);
+    updateDoc(doc(tasksCollectionRef, id), {
+    done: !tasks.value[index].done,
+  });
+}
+
 </script>
 <style lang="scss">
   body {
-    color: $dark;
-    background:#c6afc10d;
+      color: $dark;
+      background:#c6afc10d;
   }
   .done {
     .q-item__label {
@@ -128,11 +165,11 @@ export default defineComponent({
     }
   }
   .q-dialog__inner--minimized {
-    background: #96898d;
+      background: #96898d;
   }
  .q-dialog-plugin {
-       background-color: $dark;
-       color:white;
+      background-color: $dark;
+      color:white;
  }
  
 </style>
